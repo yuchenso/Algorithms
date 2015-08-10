5a82c7e408088c3212983898c23bb4e6482cc2e1
Yuchen Song <yuchensong890701@gmail.com>
2015-08-09 22:06:07 -0700

    Create Solution1.java

---
+++ Leetcod/First-Missing-Positive/Solution1.java
@@ -0,0 +1,16 @@
+public class Solution1 {
+    public int firstMissingPositive(int[] nums) {
+        if (nums == null || nums.length == 0) return 1;
+        for (int i = 0; i < nums.length; i++) {
+            while(nums[i] != i + 1 && nums[i] >= 1 && nums[i] <= nums.length && nums[nums[i] - 1] != nums[i]) {
+                int tmp = nums[i];
+                nums[i] = nums[nums[i] - 1];
+                nums[tmp - 1] = tmp;
+            }
+        }
+        for (int i = 0; i < nums.length; i++) {
+            if (nums[i] != i + 1) return i + 1;
+        }
+        return nums.length + 1;
+    }
+}
