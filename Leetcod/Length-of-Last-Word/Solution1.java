1d5e257af68f221378c2b0da290fdcbc2735c794
Yuchen Song <yuchensong890701@gmail.com>
2015-08-10 14:17:06 -0700

    Create Solution1.java

---
+++ Leetcod/Length-of-Last-Word/Solution1.java
@@ -0,0 +1,13 @@
+public class Solution1 {
+    public int lengthOfLastWord(String s) {
+        int end = s.length() - 1;
+        for (; end >= 0; end--) {
+            if (s.charAt(end) != ' ') break;
+        }
+        int start = end;
+        for (; start >= 0; start--) {
+            if (s.charAt(start) == ' ') break;
+        }
+        return end - start;
+    }
+}
