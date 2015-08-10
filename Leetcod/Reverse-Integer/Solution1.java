3a4f399644fbb3f59eeb83583e26e583799dc81d
Yuchen Song <yuchensong890701@gmail.com>
2015-08-09 21:04:00 -0700

    Create Solution1.java

---
+++ Leetcod/Reverse-Integer/Solution1.java
@@ -0,0 +1,12 @@
+public class Solution {
+    public int reverse(int x) {
+        long result = 0;
+        while(x != 0){
+            result = result * 10 + x % 10;
+            x = x / 10;
+            // handle overflow/underflow
+            if(Math.abs(result) > Integer.MAX_VALUE) return 0;
+        }
+        return (int) result;
+    }
+}
