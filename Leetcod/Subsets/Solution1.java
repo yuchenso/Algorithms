5671d1fb8bc874b4153ae7ce83f8b9a440687ff5
Yuchen Song <yuchensong890701@gmail.com>
2015-08-10 14:37:49 -0700

    Create Solution1.java

---
+++ Leetcod/Subsets/Solution1.java
@@ -0,0 +1,19 @@
+public class Solution1 {
+    public List<List<Integer>> subsets(int[] nums) {
+        Arrays.sort(nums);
+        List<List<Integer>> result = new ArrayList<List<Integer>>();
+        List<Integer> current = new ArrayList<Integer>();
+        resultBuilder(result, current, nums, 0);
+        return result;
+    }
+    
+    private void resultBuilder(List<List<Integer>> result, List<Integer> current, int[] nums, int start) {
+        result.add(new ArrayList<Integer>(current));
+    
+        for (int i = start; i < nums.length; i++) {
+            current.add(nums[i]);
+            resultBuilder(result, current, nums, i + 1);
+            current.remove(current.size() - 1);
+        }
+    }
+}
