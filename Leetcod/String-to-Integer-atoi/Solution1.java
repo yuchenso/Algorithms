e0557ae701701ecaaa044f70e9341e75736cb9b7
Yuchen Song <yuchensong890701@gmail.com>
2015-08-09 21:06:06 -0700

    Create Solution1.java

---
+++ Leetcod/String-to-Integer-atoi/Solution1.java
@@ -0,0 +1,28 @@
+public class Solution {
+    public int myAtoi(String str) {
+        str = str.trim();
+        if(str.length() == 0) return 0;
+        int index = 0;
+        int isNeg = 1;
+        if(str.charAt(0) == '-'){
+            index++;
+            isNeg = -1;
+        } 
+        if(str.charAt(0) == '+'){
+            index++;
+        }
+        
+        long result = 0;
+        for(; index < str.length(); index++){
+            if(str.charAt(index) >= '0' && str.charAt(index) <= '9'){
+                result = result * 10 + str.charAt(index) - '0';
+                if(result > Integer.MAX_VALUE) break;
+            }
+            else break;
+        }
+        
+        if(result * isNeg > Integer.MAX_VALUE) return Integer.MAX_VALUE;
+        if(result * isNeg < Integer.MIN_VALUE) return Integer.MIN_VALUE;
+        return (int)result * isNeg;
+    }
+}
