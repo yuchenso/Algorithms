0d8497a21299a36375a9fb4b9559c1e1c4ac5264
Yuchen Song <yuchensong890701@gmail.com>
2015-08-09 20:36:41 -0700

    Update Solution1.java

--- Leetcod/Two-Sum/Solution1.java
+++ Leetcod/Two-Sum/Solution1.java
@@ -1,36 +1,12 @@
-public class Solution {
-	
-    public int[] twoSum(int[] nums, int target) {
-		int[] sortedNumbers = Arrays.copyOf(nums, nums.length);
-		Arrays.sort(sortedNumbers);
-		
-		int[] indexValues = new int[2];
-		int i = 0;
-		int j = sortedNumbers.length - 1;
-		while (true) {
-			if ((sortedNumbers[i] + sortedNumbers[j]) > target) {
-				j--;
-			} else if ((sortedNumbers[i] + sortedNumbers[j]) < target) {
-				i++;
-			} else {
-				indexValues[0] = sortedNumbers[i];
-				indexValues[1] = sortedNumbers[j];
-				break;
-			}
-		}
-
-		int[] indices = new int[2];
-		int returnIndex = 0;
-		for (int k = 0; k < nums.length; k++) {
-			if (nums[k] == indexValues[0] || nums[k] == indexValues[1]) {
-				indices[returnIndex] = k + 1;
-				returnIndex++;
-			}
-			if (returnIndex == 2) {
-				break;
-			}
-		}
-		
-		return indices;
+public class SolutionClean {
+    public int[] twoSum(int[] numbers, int target) {
+        Map<Integer, Integer> map = new HashMap<Integer, Integer>();
+        for(int i = 0; i < numbers.length; i++){
+            if(map.containsKey(target - numbers[i])){
+                return new int[] {map.get(target - numbers[i]) + 1, i + 1};
+            }
+            map.put(numbers[i], i);
+        }
+        throw new IllegalArgumentException("No two sum solution");
     }
 }
