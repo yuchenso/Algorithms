bd0c6eea12c163267daa5644d9cf732d81071527
Yuchen Song <yuchensong890701@gmail.com>
2015-08-15 14:07:05 -0700

    Create Solution1.java

---
+++ Leetcod/Binary-Tree-Level-Order-Traversal-II/Solution1.java
@@ -0,0 +1,24 @@
+public class Solution1 {
+    public List<List<Integer>> levelOrderBottom(TreeNode root) {
+        List<List<Integer>> result = new ArrayList<List<Integer>>();
+        if(root == null) return result;
+        Queue<TreeNode> queue = new LinkedList<TreeNode>();
+        queue.add(root);
+        while(!queue.isEmpty()){
+            List<Integer> resultItem = new ArrayList<Integer>();
+            int num = queue.size();
+            for(int i = 0; i < num; i++){
+                TreeNode item = queue.poll();
+                resultItem.add(item.val);
+                if(item.left != null){
+                    queue.add(item.left);
+                }
+                if(item.right != null){
+                    queue.add(item.right);
+                }
+            }
+            result.add(0, resultItem);
+        }
+        return result;
+    }
+}
