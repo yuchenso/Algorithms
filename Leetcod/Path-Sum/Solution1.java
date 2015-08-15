f4a7309c2e08a568875c0dc297f59c122852b1e7
Yuchen Song <yuchensong890701@gmail.com>
2015-08-15 14:10:33 -0700

    Create Solution1.java

---
+++ Leetcod/Path-Sum/Solution1.java
@@ -0,0 +1,10 @@
+public class Solution1 {
+    public boolean hasPathSum(TreeNode root, int sum) {
+        if(root == null) return false;
+        if(root.left == null && root.right == null) {
+            if(root.val == sum) return true;
+            else return false;
+        }
+        return hasPathSum(root.left, sum - root.val) || hasPathSum(root.right, sum - root.val);
+    }
+}
